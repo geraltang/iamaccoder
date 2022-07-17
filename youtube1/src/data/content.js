a6608f55175aa030134269a4e6c1ebb071c9ff3d
geraltang <geraltangsmile@gmail.com>
2022-07-17 12:12:41 +0930

    07172

--- youtube1/src/data/content.js
+++ youtube1/src/data/content.js
@@ -2,26 +2,26 @@ const CONTENT_INFO = {
     island: {
         h1: "Never Stopping",
         h2: "Explore The World",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat "
+        p: "Lorem ipsum dolor sit consectetur adipisicing elit. Fugiat "
     },
     spring: {
         h1: "Spring",
         h2: "Feel the new vitality",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat "
+        p: "Lorem ipsum dolor sit consectetur adipisicing elit. Fugiat "
     },
     summer: {
         h1: "Summer",
         h2: "Enjoy passionate time",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat "
+        p: "Lorem ipsum dolor sit consectetur adipisicing elit. Fugiat "
     },
     autumn: {
         h1: "Autumn",
         h2: "Experience without limits",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat "
+        p: "Lorem ipsum dolor sit consectetur adipisicing elit. Fugiat "
     },
     winter: {
         h1: "Winter",
         h2: "Enjoy peace and freedom",
-        p: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat "
+        p: "Lorem ipsum dolor sit consectetur adipisicing elit. Fugiat "
     },
 }
